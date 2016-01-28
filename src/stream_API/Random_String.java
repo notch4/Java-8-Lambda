2e120e5d9e2c3a57a080ee96706aec3f88d730e5
notch4 <yubraj.pokharel555@gmail.com>
2016-01-28 10:14:02 -0600

    update

--- src/stream_API/Random_String.java
+++ src/stream_API/Random_String.java
@@ -2,6 +2,7 @@ package stream_API;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
 
 /**
@@ -12,6 +13,10 @@ public class Random_String {
         List<String> strings = Arrays.asList("I", "am", "legend");
         String[] finalString = strings.stream().toArray(String[]::new);
         System.out.println(Arrays.toString(finalString));
+
+        List<String> list = Arrays.asList("this","is","sparta");
+        Stream<Character> result = list.stream().map(s ->characterStream(s));
+
 
 
     }
