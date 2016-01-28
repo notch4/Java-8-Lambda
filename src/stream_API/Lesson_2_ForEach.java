5ba34b2138b539273664b3c22b6162d8f8871799
Yubraj Pokharel <yubraj.pokharel555@gmail.com>
2016-01-28 14:16:16 -0600

    update

--- src/stream_API/Lesson_2_ForEach.java
+++ src/stream_API/Lesson_2_ForEach.java
@@ -25,5 +25,11 @@ public class Lesson_2_ForEach {
         List<Integer> list = Arrays.asList(1,2,1,3,5,6,2,1,5,2,6,3,9);
         List<Integer> output = list.stream().map(n -> n*n).distinct().limit(3).collect(Collectors.toList());
        output.forEach(n -> System.out.println(n));
+        System.out.println("Out put as array");
+        Integer[] numbers = list.stream().map(n->n*n).distinct().limit(3).toArray(Integer[]::new);
+
+        for (Integer i:numbers) {
+                System.out.println(i);
+            }
     }
 }
