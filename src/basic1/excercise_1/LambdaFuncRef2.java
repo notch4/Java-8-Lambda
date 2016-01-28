1695c5f19124c214737cd78127c184de8a49ac4b
notch4 <yubraj.pokharel555@gmail.com>
2016-01-28 10:44:46 -0600

    update

--- src/basic1/excercise_1/LambdaFuncRef2.java
+++ src/basic1/excercise_1/LambdaFuncRef2.java
@@ -1,5 +1,8 @@
 package basic1.excercise_1;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
 import java.util.function.Consumer;
 import java.util.function.Function;
 
@@ -20,9 +23,20 @@ public class LambdaFuncRef2 {
         System.out.println(empName2.apply(new Employee("Sudhan")));
     }
 
+    public static void pickName(List<String> list, String startingLetter){
+        Optional<String> found =
+            list.stream()
+                    .filter(name -> name.startsWith(startingLetter))
+                    .findFirst();
+        found.ifPresent(name->System.out.println(name));
+    }
+
     public static void main(String[] args) {
         LambdaFuncRef2 lf2 = new LambdaFuncRef2();
         lf2.evaluater();
+        System.out.println("----------------------------------");
+        List<String> list = Arrays.asList("yubraj", "pokharel");
+        pickName(list, "yubaj");
     }
 
 
